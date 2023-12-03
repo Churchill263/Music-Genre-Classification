365449da7ee1d69cbe5885bb892e950a6843e957
Churchill263 <148724480+Churchill263@users.noreply.github.com>
2023-12-03 20:17:07 +0000

    Add files via upload

---
+++ music_genre.py
@@ -0,0 +1,30 @@
+# Import libraries
+import librosa
+import numpy as np
+from tensorflow.keras.models import load_model
+import streamlit as st
+import pickle
+
+# Load the saved model
+model = load_model("C:\\Users\\adeij\\OneDrive\\Desktop\\Music Genre Classification\\audio_classification_19_02_04.hdf5")
+
+# Define the Streamlit application
+st.title("Audio Genre Classification")
+
+# Upload audio file
+uploaded_file = st.file_uploader("Choose an audio file", type=["wav"])
+
+if uploaded_file is not None:
+    # Extract features from the uploaded file
+    audio, sample_rate = librosa.load(uploaded_file, res_type="kaiser_fast")
+    mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+    mfccs_scaled_features = np.mean(mfccs_features.T, axis=0)
+
+    # Predict genre
+    pred = model.predict(np.array([mfccs_scaled_features]))
+    predicted_class = encoder.inverse_transform(np.argmax(pred, axis=1))[0]
+
+    # Display prediction
+    st.write(f"Predicted Genre: {predicted_class}")
+else:
+    st.write("Please upload an audio file.")
